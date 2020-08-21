33b545e20b3f5814594bc1aed11620a26455a795
Jacob Paquette <40326470+EngineerJP@users.noreply.github.com>
2020-08-21 17:28:14 -0400

    Create jake.py

---
+++ jake.py
@@ -0,0 +1,17 @@
+
+name = input("Enter a male name: ")
+famous_business_owner = input("Enter name of famous business CEO: ")
+job_market = input("Enter a job market: ")
+action = input("Enter an success defining characteristic: ")
+celebrity = input("Enter a celebrity name: ")
+marketing = input("Enter a marketing action: ")
+kin = input("Enter a viable next of kin: ")
+dollar_amount = input("Enter a high dollar amount: ")
+
+print(name + " wanted to start his own business")
+print("He wanted to be a big CEO just like " + famous_business_owner)
+print("So he started his venture in " + job_market)
+print("It was very successful because of his " + action + " and his focused mindset")
+print("In addition with that, he borrowed money from " + celebrity)
+print("This money would help him grow through the use of " + marketing)
+print("In the future when he dies the " + dollar_amount + " dollar business will continue with his " + kin)
